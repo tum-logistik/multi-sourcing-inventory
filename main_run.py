aa275a7b7a287bc882e52df6ed18f903c7786d26
larkz <liu.larkin@gmail.com>
2022-06-02 18:18:20 +0200

    added ss dumb policy

--- main_run.py
+++ main_run.py
@@ -1,5 +1,6 @@
 from env.SourcingEnv import *
 from sim.sim_functions import *
+from sim.policies import *
 import numpy as np
 
 if __name__ == '__main__':
@@ -18,12 +19,17 @@ if __name__ == '__main__':
     print(str(sourcingEnv.current_state) + " cost: " + str(cost))
 
     for i in range(300):
-        random_action = np.array([np.random.randint(0, 2) for x in range(sourcingEnv.n_suppliers)])
+        
+        # totally random order amounts
+        # random_action = np.array([np.random.randint(0, 2) for x in range(sourcingEnv.n_suppliers)])
 
-        next_state, event, event_index, probs, supplier_index = sourcingEnv.step(random_action)
+        # s S policy with aribtrary s S, and randomly selected vendor
+        policy_action = ss_policy_with_randomly_select_vendor(sourcingEnv, small_s = 3, big_s = 15)
+        
+        next_state, event, event_index, probs, supplier_index = sourcingEnv.step(policy_action)
 
         cost = cost_calc(next_state, h_cost = 4, b_penalty = 6)
-        print(str(next_state) + " cost: " + str(cost) + " event: " + str(event))
+        print(str(next_state) + " cost: " + str(cost) + " event: " + str(event) + " action:" + str(policy_action))
         
         history.append([next_state, event, i, probs])
     

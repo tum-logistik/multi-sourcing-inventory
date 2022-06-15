5b0eac39d90b72fd73cd4447434c00965b3d0694
larkz <liu.larkin@gmail.com>
2022-06-15 18:26:43 +0200

    write to output

--- slave.sh
+++ slave.sh
@@ -4,5 +4,5 @@
 # docker build docker/ -t test
 # docker run --cpus=$1 --memory=$2m test
 # docker run --cpus=$1 --memory=$2m -v docker:/workspace/ -t liul/bernhard_test
-docker run --mount type=bind,source=/home/liularkin/docker/mount,target=/workspace/mount larkin/msource:latest /bin/sh -c "python workspace/mount/multi-sourcing-inventory/main.py"
+docker run --mount type=bind,source=/home/liularkin/docker/mount,target=/workspace/mount larkin/msource:latest /bin/sh -c "python workspace/mount/multi-sourcing-inventory/main.py > workspace/mount/multi-sourcing-inventory/output/run_output.txt"
 

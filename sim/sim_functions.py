61b54d9c9ef338ab3ace90de740af31ad3561cd1
larkz <liu.larkin@gmail.com>
2022-06-26 23:44:29 +0200

    added todo note

--- sim/sim_functions.py
+++ sim/sim_functions.py
@@ -7,6 +7,8 @@ def cost_calc(state, h_cost = H_COST, b_penalty = B_PENALTY):
     cost = state.s * h_cost if state.s > 0 else np.abs(state.s * b_penalty)
     return cost
 
+# TODO: Create an expected cost function
+
 
 def get_combo(y, n):
     return np.array(np.meshgrid(*[range(0, y) for x in range(n)])).T.reshape(-1, n)

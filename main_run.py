99a56975def511e13aa18e489cb46e6bd420b9c9
larkz <liu.larkin@gmail.com>
2022-06-29 17:14:20 +0200

    added safety factor to improve di performance

--- main_run.py
+++ main_run.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     #     on_times = np.array([1, 1, 2]), 
     #     off_times = np.array([0.3, 1, 0.2]))
     
-    filename = "output/msource_value_dic_06-23-2022-13-03-05.pkl"
+    filename = "output/msource_value_dic_06-25-2022-07-31-29.pkl"
 
     with open(filename, 'rb') as f:
         output_obj = pkl.load(f)
@@ -26,28 +26,28 @@ if __name__ == '__main__':
     model_params = output_obj["model_params"]
     sourcingEnv = output_obj["mdp_env"]
 
-    sourcingEnv2 = SourcingEnv(
+    sourcingEnv = SourcingEnv(
         lambda_arrival = LAMBDA, # or 10
-        procurement_cost_vec = sourcingEnv.procurement_cost_vec,
+        procurement_cost_vec = np.array([1, 3]),
         supplier_lead_times_vec = sourcingEnv.supplier_lead_times_vec,
-        on_times = sourcingEnv.on_times, 
-        off_times = sourcingEnv.off_times)
-
-    for i in range(15):
-        action_di = dual_index_policy(sourcingEnv2) if len(sourcingEnv2.action_history_tuple) > 0 else np.zeros(2)
-        sourcingEnv2.step(action_di)
-        print("step di")
-    
+        on_times = np.array([1, 1]), 
+        off_times = np.array([np.Inf, np.Inf]))
 
 
-    mc_avg_costs = mc_with_policy(sourcingEnv2,
+    mc_avg_costs = mc_with_policy(sourcingEnv, 
         periods = 30,
         nested_mc_iters = 100,
         big_s = model_params['policy_params']['big_s'],
         small_s = model_params['policy_params']['small_s'],
         h_cost = model_params['policy_params']['h_cost'],
         b_penalty = model_params['policy_params']['b_penalty'],
-        policy_callback = dual_index_policy)
+        policy_callback=dual_index_policy,
+        use_tqdm = True)
+
+    for i in range(15):
+        action_di = dual_index_policy(sourcingEnv) if len(sourcingEnv.action_history_tuple) > 0 else np.zeros(2)
+        sourcingEnv.step(action_di)
+        print("step di")
     
     # Testing dual index policy
 
--- sim/policies.py
+++ sim/policies.py
@@ -56,54 +56,59 @@ def dual_index_policy(sourcingEnv,
     b_penalty = B_PENALTY,
     big_s = BIG_S,
     small_s = SMALL_S,
-    delta_cand_range = 40):
+    delta_cand_range = 20,
+    safety_factor_di = 8):
 
     assert sourcingEnv.tracking_flag, "Assertion: Tracking feature must be on for dual index policy"
 
-    tmark_exp = sourcingEnv.get_time_mark(sourcingEnv.action_history_tuple, sourcingEnv.exp_ind)
-    tmark_reg = sourcingEnv.get_time_mark(sourcingEnv.action_history_tuple, sourcingEnv.reg_ind)
+    ord_vec = np.zeros(len(sourcingEnv.supplier_lead_times_vec))
+    # print("Opt vec: " + str(ord_vec_opt))
+    # Safety cap
+    if sourcingEnv.current_state.s >= safety_factor_di:
+        ord_vec_opt = ss_policy_fastest_supp_backlog(sourcingEnv)
+    elif sourcingEnv.current_state.s < 0:
+        ord_vec_opt = ss_policy_fastest_supp_backlog(sourcingEnv)
+        # return ord_vec_opt
+    # elif False:
+    else:
+        tmark_exp = sourcingEnv.get_time_mark(sourcingEnv.action_history_tuple, sourcingEnv.exp_ind)
+        tmark_reg = sourcingEnv.get_time_mark(sourcingEnv.action_history_tuple, sourcingEnv.reg_ind)
 
-    # Demand in the expedited range
-    demand_exp_range = np.array([x for x in sourcingEnv.demand_history_tuple if x[0] > tmark_exp])
-    cum_demand_exp_range = np.sum(demand_exp_range, axis=0)[1] if len(demand_exp_range) > 0 else 0
+        # Demand in the expedited range
+        demand_exp_range = np.array([x for x in sourcingEnv.demand_history_tuple if x[0] > tmark_exp])
+        cum_demand_exp_range = np.sum(demand_exp_range, axis=0)[1] if len(demand_exp_range) > 0 else 0
 
-    # Overshoot: defined as number of regular orders placed between n-le and n-lr
-    overshoot_range = np.array([x for x in sourcingEnv.action_history_tuple if tmark_reg < x[0] < tmark_exp])
+        # Overshoot: defined as number of regular orders placed between n-le and n-lr
+        overshoot_range = np.array([x for x in sourcingEnv.action_history_tuple if tmark_reg < x[0] < tmark_exp])
+        action_history_ov_range = np.array([x[1] for x in overshoot_range])
 
-    action_history_ov_range = np.array([x[1] for x in overshoot_range])
+        reg_orders_ov_range = action_history_ov_range[:, sourcingEnv.reg_ind] if len(action_history_ov_range) > 0 else np.array([])
+        cum_reg_orders_ov_range = np.sum(reg_orders_ov_range)
 
-    reg_orders_ov_range = action_history_ov_range[:, sourcingEnv.reg_ind] if len(action_history_ov_range) > 0 else np.array([])
-    cum_reg_orders_ov_range = np.sum(reg_orders_ov_range)
+        exp_orders_ov_range = action_history_ov_range[:, sourcingEnv.exp_ind] if len(action_history_ov_range) > 0 else np.array([])
+        cum_exp_orders_ov_range = np.sum(exp_orders_ov_range)
 
-    exp_orders_ov_range = action_history_ov_range[:, sourcingEnv.exp_ind] if len(action_history_ov_range) > 0 else np.array([])
-    cum_exp_orders_ov_range = np.sum(exp_orders_ov_range)
+        # critical fractal
+        cf = b_penalty / (b_penalty + h_cost)
+        min_cost = np.Inf
+        ze_opt = 0
+        zr_opt = 0
+        delta_opt = 0
+        gap = np.Inf
 
-    # critical fractal
-    cf = b_penalty / (b_penalty + h_cost)
-    min_cost = np.Inf
-    ze_opt = 0
-    zr_opt = 0
-    delta_opt = 0
-    ord_vec_opt = np.zeros(len(sourcingEnv.supplier_lead_times_vec))
-    gap = np.Inf
+        for del_cand in range(-delta_cand_range, delta_cand_range):
+            
+            adj_val = cum_demand_exp_range - del_cand + cum_reg_orders_ov_range
+            g_del = poisson.cdf(adj_val, sourcingEnv.lambda_arrival) #### Math Check 1 - poiss
+            perc_cand = np.min([cf, g_del]) / cf 
+            # perc_cand = (1 - g_del)  / cf
+            ze, gap = inv_poisson(perc_cand, sourcingEnv.lambda_arrival)
 
-    for del_cand in range(-delta_cand_range, delta_cand_range):
-        
-        adj_val = cum_demand_exp_range - del_cand + cum_reg_orders_ov_range
-        # ze, gap = inv_poisson(cf, sourcingEnv.lambda_arrival)
-        perc_cand = (1 - poisson.cdf(adj_val, sourcingEnv.lambda_arrival)) / cf
+            # ze, gap = inv_poisson(cf, sourcingEnv.lambda_arrival)
+            zr = np.clip(ze + del_cand, 0, np.Inf) 
 
-        ze_cand, gap = inv_poisson(perc_cand, sourcingEnv.lambda_arrival)
-
-        if np.abs(perc_cand - cf) < gap:
-            gap = np.abs(perc_cand - cf)
-            ze = np.clip(adj_val, 0, big_s)
-
-            zr = np.clip(ze + del_cand, 0, big_s) 
-
-            ord_vec = np.zeros(len(sourcingEnv.supplier_lead_times_vec))
-            ord_vec[sourcingEnv.reg_ind] = np.clip(zr - sourcingEnv.current_state.s, 0, np.Inf)
-            ord_vec[sourcingEnv.exp_ind] = np.clip(ze - sourcingEnv.current_state.s, 0, np.Inf)
+            ord_vec[sourcingEnv.exp_ind] = np.clip(ze - cum_reg_orders_ov_range, 0, np.Inf)
+            ord_vec[sourcingEnv.reg_ind] = np.clip(zr - cum_exp_orders_ov_range, 0, np.Inf)
 
             delta_cost = cost_calc_expected_di(sourcingEnv, ord_vec)
             if delta_cost < min_cost:
@@ -113,19 +118,17 @@ def dual_index_policy(sourcingEnv,
                 delta_opt = del_cand
                 ord_vec_opt = ord_vec
     
-    # print("Opt vec: " + str(ord_vec_opt))
-    # Safety cap
-    # if sourcingEnv.current_state.s >= big_s:
-    #     return np.array([0, 0])
-    # if sourcingEnv.current_state.s <= small_s:
-    #     or_vec = np.array([0, 0])
-    #     or_vec[sourcingEnv.exp_ind] = small_s - sourcingEnv.current_state.s
-    #     return or_vec
-    
+        # ord_vec_opt = np.array([0, big_s - sourcingEnv.current_state.s])
+
+        ord_vec_opt[sourcingEnv.exp_ind] = np.clip(ze_opt - sourcingEnv.current_state.s, 0, np.Inf)
+        # reg_cap = np.max([0, int(sourcingEnv.current_state.s - ord_vec[sourcingEnv.exp_ind])])
+
+        ord_vec_opt[sourcingEnv.reg_ind] = np.clip(zr_opt - sourcingEnv.current_state.s, 0, np.Inf)
+
     return ord_vec_opt
 
 
-def inv_poisson(perc, lambda_arrival, x_lim = 100, delt = 0):
+def inv_poisson(perc, lambda_arrival, x_lim = 60, delt = 0):
     gap = np.Inf
     x_opt = 0
     for x in range(x_lim):
@@ -136,27 +139,6 @@ def inv_poisson(perc, lambda_arrival, x_lim = 100, delt = 0):
     
     return x_opt, gap
 
-def x_find(dem_ov, perc_cdf, lambda_arrival, xrng = 10):
-    p_left = poisson.cdf(dem_ov, lambda_arrival)
-
-    gap = np.Inf
-    for i in range(-xrng, xrng):
-        x_cand = dem_ov + i
-        p_right = poisson.cdf(x_cand, lambda_arrival)
-        if np.abs(p_right - p_left) < gap and p_right >= p_left:
-            gap = np.abs(p_right - p_left)
-            x = x_cand
-    return x
-
-def x_search(x_cand, perc_cdf, lambda_arrival, xrng = 10):
-    delta = np.Inf
-    x = 0
-    for i in range(-xrng, xrng):
-        g_del = poisson.cdf(x_cand+i, lambda_arrival)
-        if np.abs(g_del - perc_cdf) < delta and perc_cdf - g_del > 0:
-            delta = np.abs(g_del - perc_cdf)
-            x = x_cand+i
-    return x
 
 
 # implement single supplier newsvendor,
